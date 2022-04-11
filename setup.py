492014eef028eccf8f2fbf4a83b13e43dace823e
Lysandre Debut <lysandre.debut@reseau.eseo.fr>
2022-04-11 14:59:47 -0300

    Run the scheduled tests

--- setup.py
+++ setup.py
@@ -161,7 +161,7 @@ _deps = [
     "tf2onnx",
     "timeout-decorator",
     "timm",
-    "tokenizers>=0.11.1,!=0.11.3,<0.13",
+    "tokenizers",
     "torch>=1.0,!=0.12.0",
     "torchaudio",
     "pyctcdecode>=0.3.0",
@@ -335,8 +335,7 @@ extras["dev-torch"] = (
     extras["testing"]
     + extras["torch"]
     + extras["sentencepiece"]
-    + extras["tokenizers"]
-    + extras["torch-speech"]
+    + extras["tokenizers"] + extras["torch-speech"]
     + extras["vision"]
     + extras["integrations"]
     + extras["timm"]
